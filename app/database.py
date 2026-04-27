85cabcf2d9f0548661c9c1cb6137e710590c007e
Santosh Moharir <moharirsantoshkumar@gmail.com>
2026-04-27 20:02:49 +0530

    Publish

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://claricart_user:ClariCart%40123@localhost:5432/claricart_db"
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
